87c5e69136f1665655b0270c9cc83f77ac363f7f
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-18 09:40:29 -0500

    add new book functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector('form');
 const formModal = document.querySelector('.form-modal');
 const addBook = document.querySelector('.add-book');
 const cancelBtn = document.querySelector('#cancel');
+const library = document.querySelector('.library');
 
 addBook.addEventListener('click', () => {
   formModal.style.display = 'flex';
@@ -13,4 +14,29 @@ cancelBtn.addEventListener('click', () => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  const newBook = document.createElement('div');
+  newBook.classList.add('book');
+  const title = document.querySelector('input:nth-of-type(1)').value;
+  const author = document.querySelector('input:nth-of-type(2)').value;
+  const totalPages = document.querySelector('input:nth-of-type(3)').value;
+  const pagesRead = document.querySelector('input:nth-of-type(4)').value;
+  const recommended = document.querySelector('input:nth-of-type(5)').checked;
+
+  if (Number(pagesRead) > Number(totalPages)) {
+    alert('Number of read pages cannot exceed total pages. Please try again.');
+    cancelBtn.click();
+    return;
+  }
+
+  newBook.innerHTML = `
+    <p>Book Title: <span>${title}</span></p>
+    <p>Author: <span>${author}</span></p>
+    <p>Pages Read: <span>${pagesRead} / ${totalPages}</span></p>
+    <p class="recommended">Recommended: <span>${recommended == true ? 'Yes' : 'No'}</span></p>
+    <img src="./images/trash-can-outline.png" alt="Remove from Library">`;
+    newBook.querySelector('img').addEventListener('click', () => {
+      library.removeChild(newBook);
+    });
+  library.appendChild(newBook);
+  cancelBtn.click();
 });
