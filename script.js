9bc2cb0b96f0ae5ab3a937a2733b5491fe163ce6
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-17 21:21:37 -0500

    update modal form

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const form = document.querySelector('form');
 const formModal = document.querySelector('.form-modal');
 const addBook = document.querySelector('.add-book');
+const cancelBtn = document.querySelector('#cancel');
 
 addBook.addEventListener('click', () => {
   formModal.style.display = 'flex';
+});
+
+cancelBtn.addEventListener('click', () => {
+  formModal.style.display = 'none';
 });
 
 form.addEventListener('submit', (e) => {
