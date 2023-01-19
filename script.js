54749bd4b2d11bdc02aa3497a5970a988a980852
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-19 08:51:37 -0500

    add factory function and library array

--- script.js
+++ script.js
@@ -15,26 +15,47 @@ cancelBtn.addEventListener('click', () => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  newBook();
+  const book = newBook();
+  displayBook(book);
+  myLibrary.push(book);
+  cancelBtn.click();
 });
 
 function newBook() {
-  const newBook = document.createElement('div');
-  newBook.classList.add('book');
   const title = document.querySelector('input:nth-of-type(1)').value;
   const author = document.querySelector('input:nth-of-type(2)').value;
   const totalPages = document.querySelector('input:nth-of-type(3)').value;
   const read = document.querySelector('input:nth-of-type(4)').checked;
 
+  return {
+    title,
+    author,
+    totalPages,
+    read,
+    display: true
+  }
+}
+
+function displayBook(book) {
+  const newBook = document.createElement('div');
+  newBook.classList.add('book');
+  newBook.dataset.index = myLibrary.length;
+
   newBook.innerHTML = `
-    <p>Book Title: <span>${title}</span></p>
-    <p>Author: <span>${author}</span></p>
-    <p># of Pages: <span>${totalPages}</span></p>
-    <div><label for="read-book">Read: </label><input type="checkbox" id="read-book" ${read ? 'checked="true"' : ''}></div>
+    <p>Book Title: <span>${book.title}</span></p>
+    <p>Author: <span>${book.author}</span></p>
+    <p># of Pages: <span>${book.totalPages}</span></p>
+    <div><label for="read-book">Read: </label><input type="checkbox" id="read-book" ${book.read ? 'checked="true"' : ''}></div>
     <img src="./images/trash-can-outline.png" alt="Remove from Library">`;
-    newBook.querySelector('img').addEventListener('click', () => {
-      library.removeChild(newBook);
-    });
+
+  newBook.querySelector('div').addEventListener('click', () => {
+    book.read ? book.read = false : book.read = true;
+  });
+
+  newBook.querySelector('img').addEventListener('click', () => {
+    library.removeChild(newBook);
+    book.display = false;
+  });
+
   library.appendChild(newBook);
-  cancelBtn.click();
 }
