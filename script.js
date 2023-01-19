023480efb6328f47f0a38f6fbf188f7bc14a4394
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-19 08:10:26 -0500

    edit new book cards

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const formModal = document.querySelector('.form-modal');
 const addBook = document.querySelector('.add-book');
 const cancelBtn = document.querySelector('#cancel');
 const library = document.querySelector('.library');
+let myLibrary = [];
 
 addBook.addEventListener('click', () => {
   formModal.style.display = 'flex';
@@ -23,20 +24,13 @@ function newBook() {
   const title = document.querySelector('input:nth-of-type(1)').value;
   const author = document.querySelector('input:nth-of-type(2)').value;
   const totalPages = document.querySelector('input:nth-of-type(3)').value;
-  const pagesRead = document.querySelector('input:nth-of-type(4)').value;
-  const recommended = document.querySelector('input:nth-of-type(5)').checked;
-
-  if (Number(pagesRead) > Number(totalPages)) {
-    alert('Number of read pages cannot exceed total pages. Please try again.');
-    cancelBtn.click();
-    return;
-  }
+  const read = document.querySelector('input:nth-of-type(4)').checked;
 
   newBook.innerHTML = `
     <p>Book Title: <span>${title}</span></p>
     <p>Author: <span>${author}</span></p>
-    <p>Pages Read: <span>${pagesRead} / ${totalPages}</span></p>
-    <p class="recommended">Recommended: <span>${recommended ? 'Yes' : 'No'}</span></p>
+    <p># of Pages: <span>${totalPages}</span></p>
+    <div><label for="read-book">Read: </label><input type="checkbox" id="read-book" ${read ? 'checked="true"' : ''}></div>
     <img src="./images/trash-can-outline.png" alt="Remove from Library">`;
     newBook.querySelector('img').addEventListener('click', () => {
       library.removeChild(newBook);
